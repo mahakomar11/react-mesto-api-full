3701a4b2d25737914e98bc1965a0eeef5f84f5f3
Maria Komarova <komarova@phystech.edu>
2021-04-30 14:01:49 +0300

    send token

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -86,7 +86,7 @@ const login = (req, res, next) => {
             maxAge: 3600000,
             httpOnly: true,
           })
-          .send({ token: true });
+          .send({ token });
       });
     })
     .catch(next);
