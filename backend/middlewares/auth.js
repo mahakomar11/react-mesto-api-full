e6995be51cfdf29d4322cf8a10554c80fd681529
Maria Komarova <komarova@phystech.edu>
2021-05-02 11:49:15 +0300

    check if Authorization exists

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,6 +2,7 @@ const jwt = require("jsonwebtoken");
 const CustomError = require("../utils/errors");
 
 const auth = (req, res, next) => {
+  if (!req.get('Authorization')) return next(new CustomError(401, "Необходима авторизация"));
   const token = req.get('Authorization').replace('Bearer ', '');
 
   const { NODE_ENV, JWT_SECRET } = process.env;
